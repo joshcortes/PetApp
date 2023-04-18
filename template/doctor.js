129fadeaefd9a779dcfa3d3c2c10668ed1f4b856
Christian Hart <christiantaylorhart@gmail.com>
2023-04-18 17:12:26 -0400

    Doctor's pets now display

--- template/doctor.js
+++ template/doctor.js
@@ -1,29 +1,3 @@
-// if (localStorage.getItem('token')) {
-//   loginForm = document.getElementById('loginForm');
-//   loginForm.style.display = 'none';
-//   loggedIn = document.getElementById('loggedIn');
-//   loggedIn.style.display = 'block';
-// } else {
-//   loginForm = document.getElementById('loginForm');
-//   //loginForm.style.display = "block";
-//   loggedIn = document.getElementById('loggedIn');
-//   loggedIn.style.display = 'none';
-// }
-
-// loginForm.addEventListener('submit', async () => {
-//   const username = document.getElementById('username').value;
-//   const password = document.getElementById('password').value;
-//   const data = { username: username, password: password };
-//   console.log(data);
-//   axios
-//     .post('http://localhost/doc_login', data)
-//     .then((response) => {
-//       console.log(`POST: user is added`, response.data); // append to DOM
-//       appendToDOM([response.data]);
-//     })
-//     .catch((error) => console.error(error));
-// });
-
 loginForm.addEventListener('submit', async () => {
   const data = {
     username: document.getElementById('username').value,
@@ -39,8 +13,8 @@ loginForm.addEventListener('submit', async () => {
       body: JSON.stringify(data), // body data type must match "Content-Type" header
     });
     const result = await response.json();
-    // localStorage.setItem('token', result.access_token);
-    // localStorage.setItem('user_id', result.doctor_id);
+    localStorage.setItem('token', result.access_token);
+    localStorage.setItem('user_id', result.doctor_id);
     console.log(result);
     let output = `<h1>Welcome, Dr. ${result.last_name}</h1><table>
   <tr>
