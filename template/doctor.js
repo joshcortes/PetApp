4ca186989deaf4bcb13a4748323ab0e4c1f964f3
Christian Hart <christiantaylorhart@gmail.com>
2023-04-17 23:23:15 -0400

    starting on doc_login front end fetch

--- template/doctor.js
+++ template/doctor.js
@@ -1,12 +1,40 @@
-if(localStorage.getItem('token')){
-    loginForm = document.getElementById('loginForm');
-    loginForm.style.display = "none";
-    loggedIn = document.getElementById('loggedIn');
-    loggedIn.style.display = "block";
+if (localStorage.getItem('token')) {
+  loginForm = document.getElementById('loginForm');
+  loginForm.style.display = 'none';
+  loggedIn = document.getElementById('loggedIn');
+  loggedIn.style.display = 'block';
+} else {
+  loginForm = document.getElementById('loginForm');
+  //loginForm.style.display = "block";
+  loggedIn = document.getElementById('loggedIn');
+  loggedIn.style.display = 'none';
 }
-else{
-    loginForm = document.getElementById('loginForm');
-    //loginForm.style.display = "block";
-    loggedIn = document.getElementById('loggedIn');
-    loggedIn.style.display = "none";
+
+async function doctorLogin() {
+  const response = await fetch('localhost:5000/');
+  const jsonData = await response.json();
+  console.log(jsonData);
 }
+loginForm.addEventListener('submit', async () => {
+  const username = document.querySelector('username').value;
+  const password = document.querySelector('password').value;
+  const data = [username, password];
+  console.log(data);
+  try {
+    const response = await fetch(
+      (url = 'localhost:5000/doc_login'),
+      (data = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header
+      })
+    );
+    console.log(response.json());
+    return response.json();
+  } catch (error) {
+    console.error(`Download error: ${error.message}`);
+  }
+});
