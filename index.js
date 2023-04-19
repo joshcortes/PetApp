19cb67baa52d27fbb69ae9efc273777e0b2ae4a6
Christian Hart <christiantaylorhart@gmail.com>
2023-04-19 11:02:54 -0400

    added stylings for pets, fixed background from moving

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-let map;
+let locations = [];
+let productOptions = ``;
+
+const fetchProducts = () => {
+  fetch('http://localhost:5000/get_locations')
+    .then((response) => (productResults = response.json()))
+    .then(
+      productResults.forEach((product) => {
+        productOptions += (
+          <option value='${product.product_id}'>${product.name}</option>
+        );
+      })
+    );
+};
+
+async () => {
+  const response = await fetch('http://localhost:5000/get_locations');
+  locations = await response.json();
+};
 
 async function initMap() {
   const { Map } = await google.maps.importLibrary('maps');
 
-  map = new Map(document.getElementById('map'), {
+  myMap = new Map(document.getElementById('map'), {
     center: { lat: 33.74850123320353, lng: -84.3878613378074 },
     zoom: 12,
   });
-}
 
+  fetch('http://localhost:5000/get_locations')
+    .then((response) => response.json())
+    .then((locations) => {
+      locations.forEach((location) => {
+        let marker = new google.maps.Marker({
+          position: { lat: location.lat, lng: location.lng },
+          map: myMap,
+          title: location.name,
+        });
+      });
+    });
+  marker.setMap(myMap);
+}
+// const marker = new google.maps.Marker({
+//   position: { lat: 33.75148340214221, lng: -84.32222888974691 },
+//   title: 'Feed & Seed Pet Supply',
+//   myMap,
+// });
 initMap();
