b46c440578028dca3063138bcfb9d77fa3c1d492
Chandler Dugan <duganchandler10@gmail.com>
2023-04-19 16:33:56 -0400

    added the pet symptoms and condiditons to owner

--- templates/owner.js
+++ templates/owner.js
@@ -16,7 +16,7 @@ loginForm.addEventListener('submit', async () => {
       username: document.getElementById('username').value,
       password: document.getElementById('password').value,
     };
-    console.log(data);
+    //console.log(data);
     try {
       const response = await fetch((url = 'http://localhost:5000/owner_login'), {
         method: 'POST',
@@ -63,10 +63,82 @@ loginForm.addEventListener('submit', async () => {
       console.log(petResult);
 
       // output table of pet info in the HTML DOM
-      let petList = `<h1>My Pets</h1>`;
-      petResult.forEach((pet) => {
-      petList += `<h2 class='pet' id='${pet.pet_id}'>${pet.name}  ${pet.breed_id} ${pet.age} ${pet.sex}</h2>`;
-      });
+    let pet_ids = { pet_ids: result.pet_ids };
+    console.log(result.pet_ids)
+    const pet_response = await fetch(
+      (url = 'http://localhost:5000/get_pet_symptom_condition'),
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + localStorage.getItem('token'),
+        },
+        body: JSON.stringify(pet_ids),
+      }
+    );
+
+    const pet_data = await pet_response.json();
+
+    let pet_ids_arr = pet_ids.pet_ids;
+
+    let petList = `<h1>My Pets</h1>`;
+
+    for (let i = 0; i < pet_ids_arr.length; i++) {
+      let curr_pet_id = pet_ids_arr[i];
+      let pet_conditions = pet_data[curr_pet_id].conditions;
+      let pet_symptoms = pet_data[curr_pet_id].symptoms;
+
+      let current_pet = petResult[i];
+      petList += `<h2> ${current_pet.name} ${current_pet.breed_id} ${current_pet.age} ${current_pet.sex} </h2>`;
+
+      for (let j = 0; j < pet_conditions.length; j++) {
+        let condition = pet_conditions[j];
+        let start_date = '';
+        let end_date = '';
+        if (condition.startDate != null) {
+          start_date = condition.startDate.substring(0, 16);
+        } else {
+          start_date = condition.startDate;
+        }
+
+        if (condition.endDate != null) {
+          end_date = condition.endDate.substring(0, 16);
+        } else {
+          end_date = 'N/A';
+        }
+
+        petList += `<h3 title="${condition.description}">
+        <span>Condition:</span> ${condition.name}, 
+        ${condition.severity} 
+        <span>Start: </span>${start_date} 
+        <span>End: </span>${end_date}</h3>`;
+        console.log(condition.condition_id);
+      }
+
+      for (let k = 0; k < pet_symptoms.length; k++) {
+        let symptom = pet_symptoms[k];
+        start_date = '';
+        end_date = '';
+        if (symptom.startDate != null) {
+          start_date = symptom.startDate.substring(0, 16);
+        } else {
+          start_date = symptom.startDate;
+        }
+
+        if (symptom.endDate != null) {
+          end_date = symptom.endDate.substring(0, 16);
+        } else {
+          end_date = 'N/A';
+        }
+        console.log(start_date);
+        petList += `<h3><span>Symptom: </span>${symptom.name} 
+        ${symptom.severity}
+        <span>Start: </span>${start_date} 
+        <span>End: </span>${end_date}<h3>`;
+      }
+    }
+    //document.getElementById('your-pets').innerHTML = petList;
+      
       document.getElementById('pets').innerHTML = petList;
     } catch (error) {
       document.getElementById(
